bcfdd025f304f6c33a14998862f5c32db912d88d
Angelina-te <113330744+Angelina-te@users.noreply.github.com>
2022-10-19 03:33:13 +0700

    2

---
+++ 2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Начальное число M: ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.Write("Начальное число M: ");
+int numberN = int.Parse(Console.ReadLine());
+
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.Write($"Сумма элементов в промежутках  M до N: {sum}" ); 
+       
+        return;
+    }
+    
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
